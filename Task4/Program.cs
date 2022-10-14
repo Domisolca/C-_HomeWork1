a2509b3e6740ff458cd8829157c6a0d42c9afea5
Olga <olya_potapova@mail.ru>
2022-10-14 09:19:49 +0300

    Add Task 4

---
+++ Task4/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number < 1)
+{
+    Console.WriteLine("Число меньше 1!");
+}
+else
+{
+    int currentNumber = 1;
+
+    while(currentNumber <= number)
+    {
+        if(currentNumber % 2 == 0)
+        {
+            Console.Write(currentNumber + " ");
+        }
+        
+        currentNumber++;
+    }
+}
