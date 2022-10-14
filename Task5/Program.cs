bf75ccae6fac30446cf8b0c1c80fa2e5c65b59fc
Olga <olya_potapova@mail.ru>
2022-10-14 10:15:39 +0300

    Add Task 5

---
+++ Task5/Program.cs
@@ -0,0 +1,19 @@
+﻿string massage = "Секретное сообщение 1";
+string password = "12345";
+
+int i = 1;
+bool Flag = true;
+
+while(i <= 3 && Flag)
+{
+    Console.Write("Введите пароль: ");
+    string userPassword = Console.ReadLine();
+
+    if(userPassword == password)
+    {
+        Console.WriteLine(massage);
+        Flag = false;
+    }
+
+    i++;
+}
