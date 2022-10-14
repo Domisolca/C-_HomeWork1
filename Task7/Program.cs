01cd122e295d6951bf5127745dafbf017fb39fc6
Olga <olya_potapova@mail.ru>
2022-10-14 12:54:12 +0300

    Add Task 7

---
+++ Task7/Program.cs
@@ -0,0 +1,27 @@
+﻿int min = 1, max = 10, number = new Random().Next(min, max);
+bool flagContinue = true;
+int i = 1;
+
+Console.WriteLine(number);
+
+while (i <= 3 && flagContinue)
+{
+    Console.Write("Введите число в интервале [" + min + "; " + max + "]: ");
+    int userNumber = Convert.ToInt32(Console.ReadLine());
+
+    if (userNumber == number)
+    {
+        Console.WriteLine("Вы угадали число!");
+        flagContinue = false;
+    }
+    else if (userNumber < number)
+    {
+        Console.WriteLine("Ваше число меньше загаданного!");
+    }
+    else
+    {
+        Console.WriteLine("Ваше число больше загаданного!");
+    }
+
+    i++;
+}
