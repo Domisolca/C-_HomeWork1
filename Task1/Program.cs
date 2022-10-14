aa623c0d2fe77444b24129136fd4fae2babd937e
Olga <olya_potapova@mail.ru>
2022-10-14 09:00:28 +0300

    Add Task 1

---
+++ Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число А: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if(numberA > numberB)
+{
+    Console.Write("max = " + numberA + "; min = " + numberB);
+}
+else
+{
+    Console.Write("max = " + numberB + "; min = " + numberA);
+}
